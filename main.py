2f912029c63f894562cb3f4529b5025cb11ba04b
haridhrasuresh <69705746+haridhrasuresh@users.noreply.github.com>
2026-04-08 15:14:37 +0200

    updated main
    - only calls relevant plots
    - other plots retained for reference

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import logging
 from data_loader import LiteratureDataset
 from visualization.year_category_pies import plot_yearly_category_pies
 from visualization.count_category_over_years import count_category_over_years
-from visualization.bubble_approach import plot_category_approach_bubble
-from visualization.word_cloud_search import plot_keyword_wordcloud
 from visualization.sankey_flow_complex import plot_modality_approach_category_sankey
-from visualization.hierarchy import plot_category_approach_hierarchy
 from visualization.count_category_over_years import count_category_over_years
-from visualization.stacked_category_approach import count_approach_over_category
 from visualization.choloropleth_bee import plot_choropleth_country
 from visualization.ridge_category_years import ridge_plot_approaches_over_years
 from visualization.bee_demographic_bar import plot_bar_bee_demographic
 from visualization.demographic_migrations import plot_circular_migration_bee_research
-####Configure logging
+###Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -29,16 +25,8 @@ def main():
     plot_yearly_category_pies(df)
     logger.info("Generating: Count Category Over Years")
     count_category_over_years(df)
-    logger.info("Generating: Category Approach Bubble Plot")
-    plot_category_approach_bubble(df)
-    logger.info("Generating: Keyword Word Cloud")
-    plot_keyword_wordcloud(df)
     logger.info("Generating: Modality Approach Category Sankey")
     plot_modality_approach_category_sankey(df)
-    logger.info("Generating: Category Approach Hierarchy")
-    plot_category_approach_hierarchy(df)
-    logger.info("Generating: Stacked Approach Over category")
-    count_approach_over_category(df)
     logger.info("Generating: Choropleth Country")
     plot_choropleth_country(df)
     logger.info("Generating: Bar Plot - Bee Demographic")
@@ -47,6 +35,7 @@ def main():
     plot_circular_migration_bee_research(df)
     logger.info("Generating: Ridge Plot - Approaches Over Years")
     ridge_plot_approaches_over_years(df)
+    
 
 if __name__ == "__main__":
     main()
